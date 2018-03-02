5ede37f641a3b8cb870e5c6e53b5a806b6098e10
spooky <spooky9@poczta.onet.pl>
2018-03-02 23:39:29 +0100

    batching

--- lineendings.go
+++ lineendings.go
@@ -46,21 +46,51 @@ func countEndings(s string) Endings {
 	return Endings{uint(x), uint(y - x)}
 }
 
-func main() {
-	searchDir := "./data" //os.Args[1]
-	c := make(chan Endings)
-
-	fileList := getFileList(searchDir)
-	for _, file := range fileList {
-		go getFileEndings(file, c)
+func splitIntoChunks(arr []string, chunkSize int) [][]string {
+	if chunkSize <= 0 {
+		panic("chunk size too small")
 	}
 
-	count := Endings{0, 0}
-	for _ = range fileList {
-		result := <-c
+	if len(arr) <= chunkSize {
+		return [][]string{arr}
+	}
 
-		count.crlf += result.crlf
-		count.lf += result.lf
+	numChunks := int(len(arr)/chunkSize) + 1
+	chunks := make([][]string, numChunks)
+
+	for i := 0; i < numChunks; i++ {
+		l := i * chunkSize
+		u := l + chunkSize
+
+		if u > len(arr) {
+			u = len(arr)
+		}
+
+		chunks[i] = arr[l:u]
+	}
+
+	return chunks
+}
+
+func main() {
+	searchDir := os.Args[1]
+	c := make(chan Endings)
+	chunkSize := 1000
+
+	fileList := getFileList(searchDir)
+
+	count := Endings{0, 0}
+	for _, chunk := range splitIntoChunks(fileList, chunkSize) {
+		for _, file := range chunk {
+			go getFileEndings(file, c)
+		}
+
+		for _ = range chunk {
+			result := <-c
+
+			count.crlf += result.crlf
+			count.lf += result.lf
+		}
 	}
 
 	fmt.Printf("crlf: %d, lf: %d\n", count.crlf, count.lf)
